74f219b4475a72e616102fe665745f18200fcbf9
Karim MuHamad <karimmuhamadfci@gmail.com>
2020-04-22 20:36:25 +0200

    Add User Model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,38 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    
+    username: {
+        type: String,
+        unique: true
+    },
+    fullName: {
+        type: String,
+        unique: true,
+        default: ''
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+        default: '' // default we be the value if the user sign in with facebook or google account
+    },
+    userImage: {
+        type: String,
+        default: 'default.png'
+    },
+    // Oauth
+    facebook: {
+        type: String,
+        default: ''
+    },
+    fbTokens: Array,
+    google: {
+        type: String,
+        default: ''
+    },
+    googleTokens: Array
 });
 
 module.exports = mongoose.model('User', userSchema);
