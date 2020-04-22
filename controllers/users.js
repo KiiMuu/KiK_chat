6184de0af5caf7a2a406bd35a9b809e610984cc4
Karim MuHamad <karimmuhamadfci@gmail.com>
2020-04-22 21:55:03 +0200

    SignUp Post Route

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,9 @@
+const passport = require('passport');
+
+exports.homePage = (req, res, next) => {
+    res.render('home');
+}
+
 exports.indexPage = (req, res, next) => {
     res.render('index', {
         test: 'This is a Test'
@@ -6,4 +12,12 @@ exports.indexPage = (req, res, next) => {
 
 exports.getSignUp = (req, res, next) => {
     res.render('auth/signup');
+}
+
+exports.postSignUp = (req, res, next) => {
+    passport.authenticate('local.signup', {
+        successRedirect: '/home',
+        failureRedirect: 'auth/signup',
+        failureFlash: true
+    });
 }
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,12 @@ const usersController = require('../controllers/users');
 
 const router = express.Router();
 
+// home
+router.get('/home', usersController.homePage);
+// index
 router.get('/', usersController.indexPage);
+// sign up
 router.get('/signup', usersController.getSignUp);
+router.post('/signup', usersController.postSignUp);
 
 module.exports = router;
