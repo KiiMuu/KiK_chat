6cdeec23d5ddb1f47b0a3010148e32489ad4c629
Karim MuHamad <karimmuhamadfci@gmail.com>
2020-05-03 09:07:35 +0200

    Listen for friend request event

--- public/js/sendrequest.js
+++ public/js/sendrequest.js
@@ -1,3 +1,42 @@
 $(document).ready(() => {
     let socket = io();
+
+    let room = $('#groupName').val();
+    let sender = $('#sender').val();
+
+    socket.on('connect', () => {
+        let params = {
+            sender
+        }
+
+        socket.emit('joinRequest', params, () => {
+            console.log('joined');
+        });
+    });
+
+    socket.on('newFriendRequest', friend => {
+        console.log(friend);
+    });
+
+    $('#add-friend').on('submit', e => {
+        e.preventDefault();
+
+        let recieverName = $('#recieverName').val();
+
+        $.ajax({
+            url: `group/${room}`,
+            type: 'POST',
+            data: {
+                recieverName
+            },
+            succes: () => {
+                socket.emit('friendRequest', {
+                    reciever: recieverName,
+                    sender
+                }, () => {
+                    console.log('Request sent');
+                });
+            }
+        });
+    });
 });
--- socket/friend.js
+++ socket/friend.js
@@ -1,5 +1,18 @@
 module.exports = io => {
     io.on('connection', socket => {
-        
+        socket.on('joinRequest', (myRequest, cb) => {
+            socket.join(myRequest.sender);
+
+            cb();
+        });
+
+        socket.on('friendRequest', (friend, cb) => {
+            io.to(friend.reciever).emit('newFriendRequest', {
+                from: friend.sender,
+                to: friend.reciever
+            });
+
+            cb();
+        });
     });
 }
